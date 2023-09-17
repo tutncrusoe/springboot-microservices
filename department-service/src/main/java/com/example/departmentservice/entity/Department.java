8aaa29da4f7d1e66e173545be825a14a00fefacd
tutncrusoe <huydinhanhle@gmail.com>
2023-09-17 23:41:35 +0700

    create Department and DepartmentRepository

---
+++ department-service/src/main/java/com/example/departmentservice/entity/Department.java
@@ -0,0 +1,28 @@
+package com.example.departmentservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String departmentName;
+
+    private String departmentDescription;
+
+    private String departmentCode;
+
+}
