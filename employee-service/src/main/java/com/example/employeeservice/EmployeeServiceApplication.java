534ade47e5a04462896d92038f99488b9dd02a2e
tutncrusoe <huydinhanhle@gmail.com>
2023-09-26 18:53:56 +0700

    config eureka-client employee-service

--- employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/example/employeeservice/EmployeeServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
